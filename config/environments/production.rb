c84888ebdd947dd0a334e89ce7fa4b6dcc9781b5
mushky <msazaidi@gmail.com>
2013-11-11 01:02:46 -0800

    Configured Devise, added sign in and log out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Set to actual host for devise
+  config.action_mailer.default_url_options = { :host => 'http://seriouslypinteresting.herokuapp.com/' }
 end
